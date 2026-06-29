ea8fc685af1864eebf1288cbc3b0b57d797ed1a4
Ahmad Sana <saahmed311@gmail.com>
2026-06-29 18:03:50 +0530

    Move Firebase credentials to environment variables

--- backend/src/utils/firebase.ts
+++ backend/src/utils/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIdD8Jg2J0c93Fs0xfrLkLjwq1Nd77n1c",
-  authDomain: "quicklink-shortener.firebaseapp.com",
-  projectId: "quicklink-shortener",
-  storageBucket: "quicklink-shortener.firebasestorage.app",
-  messagingSenderId: "959592304030",
-  appId: "1:959592304030:web:2551863dfd5699c6145a8f",
-  measurementId: "G-NLELHE49TT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
